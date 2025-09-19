9e03fa2af80d8c58c2483228dc6027a6d75d6cfd
PooyaServer <153263162+pooyaserver@users.noreply.github.com>
2025-09-19 23:24:48 +0330

    Update main.sh

--- main.sh
+++ main.sh
@@ -45,9 +45,8 @@ auto_detect_ip() {
 
 banner() {
   clear
-  SERVER_IP=$(hostname -I | awk '{print $1}')
-  SERVER_COUNTRY=$(curl -sS "http://ip-api.com/json/$SERVER_IP" | jq -r '.country')
-  SERVER_ISP=$(curl -sS "http://ip-api.com/json/$SERVER_IP" | jq -r '.isp')
+  SERVER_COUNTRY=$(curl -sS "http://ip-api.com/json/$(auto_detect_ip)" | jq -r '.country')
+  SERVER_ISP=$(curl -sS "http://ip-api.com/json/$(auto_detect_ip)" | jq -r '.isp')
   echo "======================================================"
   echo "             VXLAN Tunnel Manager v1.0"
   echo "             PooyaServerSup"
